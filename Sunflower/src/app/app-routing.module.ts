945e15f986d6d3e8294c53edadb4ea14aa326eea
Leonardo Maiorano <leonardomaiorano@outlook.com.br>
2022-02-21 16:12:51 -0300

    add component cadastrar e entrar

--- Sunflower/src/app/app-routing.module.ts
+++ Sunflower/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'entrar', pathMatch: 'full'},
   
   {path: 'entrar', component: EntrarComponent},
-  {path: 'cadastrar', component: CadastrarComponent}
+  {path: 'cadastrar', component: CadastrarComponent},
+  {path: 'sobre', component: SobreComponent}
 
 ];
 
